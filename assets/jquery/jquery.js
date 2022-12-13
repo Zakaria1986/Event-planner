d9b52b19d9af7b2b95494570ab59bcbc9a3acf70
Zak khan <zakaria2012@gmail.com>
2022-12-13 20:55:57 +0000

    Manage to get loop working next need to creat the html and add the time and input and the save buttons

--- assets/jquery/jquery.js
+++ assets/jquery/jquery.js
@@ -5,6 +5,8 @@
 **  SO THAT I can manage my time effectively
 */
 
+//const { holdReady } = require("jquery");
+
 // ## Acceptance Criteria
 
 //  Display the current day at the top of the calender when a user opens the planner.
@@ -12,8 +14,28 @@ var currentDay = moment().format('dddd MMM Do');;
 document.getElementById("currentDay").innerHTML = currentDay;
 
 // Present timeblocks for standard business hours when the user scrolls down.
-    // - buniness hours are 9-5:30
-    // -
+// - got every hour create a time block row from  9-5
+//  - To do that need loop to start looking from 9 and break at 5
+//  - For every lool generate html and check if its = or greate then break
+
+// var hours = moment(09, HH);
+var startOfBusiness = moment(09, 'hh');
+
+while (startOfBusiness.hour() < 18) {
+    console.log(startOfBusiness.format('hha'));
+
+    startOfBusiness.add(1, 'hours');
+
+    console.log(moment().format("HH").isBefore(startOfBusiness));
+}
+
+// for (i = 9; i > 4; --i) {
+//     console.log(i + hours);
+// }
+
+
+// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+ // Use isBefore method in moment to check the moments
 
 
 //  * Allow a user to enter an event when they click a timeblock
